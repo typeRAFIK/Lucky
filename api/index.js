7b4cdecde836555c3729d7d2240a41d937cbdb42
Iskender <kublisken@gmail.com>
2025-05-28 20:12:18 +0600

    Fix project structure for Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/', (req, res) => {
+  res.json({ message: 'Привет от LuckyZone!' });
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+module.exports.handler = serverless(app);
